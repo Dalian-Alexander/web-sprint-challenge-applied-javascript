5ea111eff3b2495d5a5a436b59521f5131d9d811
Dalian <DalianAlyxander@me.com>
2021-06-12 20:42:42 -0400

    complete and passed tests for task0 & task 1

--- src/components/header.js
+++ src/components/header.js
@@ -11,6 +11,29 @@ const Header = (title, date, temp) => {
   //    <span class="temp">{ temp }</span>
   //  </div>
   //
+
+  //creating elements
+  const divHeader = document.createElement('div')
+  const spanDate = document.createElement('span')
+  const h1Title = document.createElement('h1')
+  const spanTemp = document.createElement('span')
+
+  //adding classes to elements
+  divHeader.classList.add('header')
+  spanDate.classList.add('date')
+  spanTemp.classList.add('temp')
+
+  //attaching text content
+  spanDate.textContent = `${date}`
+  h1Title.textContent = `${title}`
+  spanTemp.textContent = `${temp}`
+
+  //creating hierarchy markup
+  divHeader.appendChild(spanDate)
+  divHeader.appendChild(h1Title)
+  divHeader.appendChild(spanTemp)
+
+  return divHeader
 }
 
 const headerAppender = (selector) => {
